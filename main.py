5b3901f1f30b08d2149238a866c18ca6df3fd3d2
Adam DiCioccio <adicioccio@argoblockchain.com>
2023-10-14 18:40:31 -0400

    added more readable output message

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ csv_files = ['data/draftkings.csv', 'data/bet365.csv', 'data/fanduel.csv']
 output_name = 'data/output.csv'
 calculate_csv = 'data/output.csv'
 
+return_num = 100
+
 def merge_csv(csv_files, output):
     data = pd.DataFrame()
     for file in csv_files:
@@ -22,14 +24,14 @@ def calc_dec(american):
 def calc(csv_file):
     with open(csv_file, 'r') as csvfile:
         rows = csv.reader(csvfile)
-        total = 0
+        total_cost = 0
         for row in rows:
             if row[0] == 'book':
                 continue
             num = int(row[2].replace("+", ""))
-            bet = round(100 / calc_dec(num), 2)
-            total += bet
-        print(total)
+            bet = round(return_num / calc_dec(num), 2)
+            total_cost += bet
+        print(f"Total bet: ${total_cost} | Total return: ${return_num}")
 merge_csv(csv_files, output_name)
 
 calc(calculate_csv)
